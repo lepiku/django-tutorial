4414ed5e65a40b712c1bfe3677b9d03a7fbaef51
dimasokto <>
2018-12-21 00:19:14 +0700

    similar to unittest

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
-    path('<int:question_id>/results/', views.results, name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('polls/', views.IndexView.as_view(), name='index'),
+    path('polls/<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('polls/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('polls/<int:question_id>/vote/', views.vote, name='vote'),
 ]
